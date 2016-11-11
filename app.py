7c2d44fe5183371a663984a2f2125c79090c6d94
Cheyan Setayesh <cheyan.set@gmail.com>
2016-11-10 22:47:57 -0500

    temp fix ssl errors, fix vote db insertion

--- app.py
+++ app.py
@@ -7,8 +7,4 @@ os.environ['DEBUG'] = '1'
 app.secret_key = os.environ['SECRET_KEY']
 app.config['SESSION_TYPE'] = 'filesystem'
 
-app.run(host="0.0.0.0", port=int(os.environ['PORT']),
-        debug=False, ssl_context=(
-            os.environ['CERT_PATH'],
-            os.environ['KEY_PATH']
-        ))
+app.run(host="0.0.0.0", port=int(os.environ['PORT']), debug=False)
